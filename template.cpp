39174c081823bb2df9418091fa1425f9f5a17a4a
dqn <suyarizzz@gmail.com>
2020-03-01 20:33:38 +0900

    update

--- template.cpp
+++ template.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+
+#define range(i, begin, end) for (int i = begin; i < end; i++)
+#define rep(i, n) range(i, 0, n)
+#define input(x) cin >> x;
+#define inputn(x, n) x.resize(n); rep(i, n) cin >> x[i];
+#define print(x) cout << x << endl;
+
 using namespace std;
-typedef long long ll;
+using ll = long long;
+
+int n;
+vector<int> a;
 
 int main() {
-  
+  input(n);
+  inputn(a, n);
+
+  int ans = 0;
+
+  print(ans);
 }
