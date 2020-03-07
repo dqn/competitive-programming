ac3ad3e852eca38cbd8c4ab1a5349498736b5a93
dqn <suyarizzz@gmail.com>
2020-03-08 08:34:40 +0900

    update

--- atcoder/abc152/c.cpp
+++ atcoder/abc152/c.cpp
@@ -8,7 +8,7 @@ using pii = pair<int, int>;
 
 #define INF 1e9
 #define MOD 1000000007
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
 #define each(x, v) for (auto& x: (v))
 #define all(x) (x).begin(), (x).end()
 #define print(x) cout << (x) << endl
@@ -18,5 +18,15 @@ void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u
 int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
 
 void solve() {
-  
+  int N;
+  in(N);
+  int ans = 0;
+  int mn = INF;
+  rep(i, N) {
+    int pi;
+    in(pi);
+    if (pi <= mn) ans++;
+    mn = min(mn, pi);
+  }
+  print(ans);
 }
--- atcoder/abc152/d.cpp
+++ atcoder/abc152/d.cpp
@@ -8,7 +8,7 @@ using pii = pair<int, int>;
 
 #define INF 1e9
 #define MOD 1000000007
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep(i, a, n) for (int i = a; i < (n); i++)
 #define each(x, v) for (auto& x: (v))
 #define all(x) (x).begin(), (x).end()
 #define print(x) cout << (x) << endl
@@ -17,6 +17,22 @@ void solve();
 void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u...); }
 int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
 
+int c[10][10];
+
 void solve() {
-  
+  int N;
+  in(N);
+  rep(i, 1, N + 1) {
+    int a = i % 10;
+    int b = i;
+    while (b >= 10) b /= 10;
+    c[a][b]++;
+  }
+  int ans = 0;
+  rep(i, 1, 10) {
+    rep(j, 1, 10) {
+      ans += c[i][j] * c[j][i];
+    }
+  }
+  print(ans);
 }
