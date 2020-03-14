11366aafc208ccdfb316d32a118ca0d4e83e2c58
dqn <suyarizzz@gmail.com>
2020-03-14 16:19:15 +0900

    update

---
+++ atcoder/abc137/c.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define INF 1e9
+#define MOD 1000000007
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a); i > (b); i--)
+#define each(x, v) for (auto& x: (v))
+#define all(x) (x).begin(), (x).end()
+#define print(x) cout << (x) << endl
+
+void solve();
+void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u...); }
+int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
+
+void solve() {
+  int N;
+  in(N);
+  map<string, int> m;
+  ll ans = 0;
+  rep(i, 0, N) {
+    string si;
+    in(si);
+    sort(all(si));
+    ans += m[si]++;
+  }
+  print(ans);
+}
