ce7f9f8e6f81262c908011cf6c14c228c62cfba3
dqn <suyarizzz@gmail.com>
2020-03-14 15:15:43 +0900

    update

---
+++ atcoder/abc139/c.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define INF 1e9
+#define MOD 1000000007
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a); i > (b); i--)
+#define each(x, v) for (auto& x: (v))
+#define all(x) (x).begin(), (x).end()
+#define print(x) cout << (x) << endl
+
+void solve();
+void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u...); }
+int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
+
+void solve() {
+  int N;
+  in(N);
+  vi H(N);
+  each(x, H) in(x);
+  int ans = 0;
+  int tmp = 0;
+  rep(i, 0, N - 1) {
+    if (H[i + 1] > H[i]) {
+      tmp = 0;
+    } else {
+      ans = max(ans, ++tmp);
+    }
+  }
+  print(ans);
+}
