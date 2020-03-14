b2b321b6bfa59752851e35b8131a7dd540194ae0
dqn <suyarizzz@gmail.com>
2020-03-14 20:47:17 +0900

    update

---
+++ atcoder/abc131/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define INF 1e9
+#define MOD 1000000007
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a); i > (b); i--)
+#define each(x, v) for (auto& x: (v))
+#define all(x) (x).begin(), (x).end()
+#define print(x) cout << (x) << endl
+
+void solve();
+void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u...); }
+int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
+
+ll gcd(ll a, ll b) {
+  while(a^=b^=a^=b%=a);
+  return b;
+}
+
+void solve() {
+  ll A, B, C, D;
+  in(A, B, C, D);
+  ll ans = B - A + 1;
+  ans -= (B / C) - ((A - 1) / C);
+  ans -= (B / D) - ((A - 1) / D);
+  ll l = C * D / gcd(C, D);
+  ans += (B / l) - ((A - 1) / l);
+  print(ans);
+}
--- utils/lcm.cpp
+++ utils/lcm.cpp
@@ -2,3 +2,7 @@ int gcd(int a, int b) {
   while(a^=b^=a^=b%=a);
   return b;
 }
+
+int lcm(int a, int b) {
+  return (a * b) / gcd(a, b);
+}
