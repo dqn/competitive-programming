9ad373de1e9b903e81fb2e09fe24407b9aea2d6d
dqn <suyarizzz@gmail.com>
2020-03-14 16:48:37 +0900

    update

--- atcoder/abc136/c.cpp
+++ atcoder/abc136/c.cpp
@@ -19,5 +19,17 @@ void in(){} template<class T, class... U> void in(T &t, U &...u){ cin >> t; in(u
 int main() { cin.tie(0); ios::sync_with_stdio(0); solve(); }
 
 void solve() {
-  
+  int N;
+  in(N);
+  vi H(N);
+  each(x, H) in(x);
+  rrep(i, N - 2, -1) {
+    if (H[i] > H[i + 1]) {
+      if (--H[i] != H[i + 1]) {
+        print("No");
+        return;
+      }
+    }
+  }
+  print("Yes");
 }
