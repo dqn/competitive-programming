0f24e322b2aa7f65af3550ed53f2b0f897652a72
dqn <suyarizzz@gmail.com>
2020-03-15 10:27:09 +0900

    update

---
+++ atcoder/abc126/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using pii=pair<int,int>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);in(name)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(x) cout<<(x)<< endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+void solve() {
+  VAR(ll, N, K);
+  double ans = 0;
+  rep(i, 0, N) {
+    int cnt = 0;
+    ll p = i + 1;
+    while (p < K) {
+      p *= 2;
+      cnt++;
+    }
+    ans += 1/pow(2, cnt);
+  }
+  printf("%.12f", ans / (double)N);
+}
