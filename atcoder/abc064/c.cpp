4f01c295be30dec2a8d0b470aa2cd4439e40abba
dqn <suyarizzz@gmail.com>
2020-03-15 22:39:37 +0900

    update

---
+++ atcoder/abc064/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using pii=pair<int,int>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(x) cout<<(x)<<endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+void solve() {
+  VAR(int, N);
+  VEC(int, a, N);
+  vi m(8);
+  int free = 0;
+  each(x, a) {
+    if (x >= 3200) {
+      free++;
+    } else {
+      m[x / 400] = 1;
+    }
+  }
+  int sum = accumulate(all(m), 0);
+  cout << max(sum, 1) << ' ' << sum + free << endl;
+}
