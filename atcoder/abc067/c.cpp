e4b880126c415a2a5319aa685a0ca18d16c9fe4f
dqn <suyarizzz@gmail.com>
2020-03-16 00:03:00 +0900

    update

---
+++ atcoder/abc067/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using pii=pair<int,int>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(x) cout<<(x)<<endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+void solve() {
+  VAR(ll, N);
+  VEC(ll, a, N);
+  vector<ll> acc(N);
+  acc[0] = a[0];
+  rep(i, 1, N) {
+    acc[i] = acc[i - 1] + a[i];
+  }
+  ll mn = LINF;
+  rep(i, 0, N - 1) {
+    mn = min(mn, abs(acc[i] - (acc[N - 1] - acc[i])));
+  }
+  print(mn);
+}
