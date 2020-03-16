fcadb14d737965cc2a6267827193ba7113bdbca1
dqn <suyarizzz@gmail.com>
2020-03-16 14:53:18 +0900

    update

---
+++ atcoder/abc086/c.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using pii=pair<int,int>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(x) cout<<(x)<<endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+void solve() {
+  VAR(int, N);
+  vi t(N), x(N), y(N);
+  rep(i, 0, N) {
+    in(t[i], x[i], y[i]);
+  }
+  int nt = 0, nx = 0, ny = 0;
+  rep(i, 0, N) {
+    int d = abs(nx - x[i]) + abs(ny - y[i]);
+    int dt = t[i] - nt;
+    if (d > dt || (dt - d) % 2) {
+      print("No");
+      return;
+    }
+    nx = x[i];
+    ny = y[i];
+    nt = t[i];
+  }
+  print("Yes");
+}
