e3a2f7b21d1ce3fb7dc8187474cb4b35223a3419
dqn <suyarizzz@gmail.com>
2020-03-16 23:08:36 +0900

    update

--- utils/comb.cpp
+++ utils/comb.cpp
@@ -1,16 +1,2 @@
-ll power(ll a, ll n) {
-  ll ret = 1;
-  for (; n > 0; n >>= 1, a = a * a % MOD) {
-    if (n % 2 == 1) ret = ret * a % MOD;
-  }
-  return ret;
-}
-
-ll comb(ll n, ll r) {
-  ll x = 1, y = 1;
-  rep(i, r) {
-    x = x * (n - i) % MOD;
-    y = y * (i + 1) % MOD;
-  }
-  return x * power(y, MOD - 2) % MOD;
-}
+ll power(ll a,ll n){ll ret=1;for(;n>0;n>>=1,a=a*a%MOD)if(n%2==1)ret=ret*a%MOD;return ret;}
+ll comb(ll n,ll r){ll x=1,y=1;rep(i,0,r){x=x*(n-i)%MOD;y=y*(i+1)%MOD;}return x*power(y,MOD-2)%MOD;}
