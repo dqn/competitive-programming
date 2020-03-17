f4cc1ae31d26e33dd4b18b75f473696d6f4e4a58
dqn <suyarizzz@gmail.com>
2020-03-18 00:56:07 +0900

    update

--- utils/power.cpp
+++ utils/power.cpp
@@ -1,7 +1,1 @@
-ll power(ll a, ll n) {
-  ll ret = 1;
-  for (; n > 0; n >>= 1, a = a * a % MOD) {
-    if (n % 2 == 1) ret = ret * a % MOD;
-  }
-  return ret;
-}
+ll power(ll a,ll n){ll ret=1;for(;n>0;n>>=1,a=a*a%MOD)if(n%2==1)ret=ret*a%MOD;return ret;}
