850a9ea4ca365b5fe0d60faa2977943974f29c78
dqn <suyarizzz@gmail.com>
2020-03-18 13:39:10 +0900

    update

---
+++ atcoder/abc045/c.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(x) cout<<(x)<<endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+void solve() {
+  VAR(string, S);
+  ll ans = 0;
+  int s = S.length();
+  rep(i, 1, s + 1) {
+    rep(j, 0, s - i + 1) {
+      ll val = stoll(S.substr(j, i));
+      int t = pow(2, max(j - 1, 0) + max(s - (j + i) - 1, 0));
+      ans += val * t;
+    }
+  }
+
+  print(ans);
+}
