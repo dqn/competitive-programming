ede94505b66c2e6ffa493922292a52bf95cb350f
dqn <suyarizzz@gmail.com>
2020-03-22 20:51:40 +0900

    update #1

--- atcoder/abc128/c.cpp
+++ atcoder/abc128/c.cpp
@@ -3,54 +3,59 @@ using namespace std;
 using ll=long long;
 using vi=vector<int>;
 using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
 using pii=pair<int,int>;
+using pll=pair<ll,ll>;
 const ll LINF=0x1fffffffffffffff;
 const int INF=0x3fffffff;
 const int MOD=1000000007;
 #define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
-#define VEC(type,name,size) vector<type> name(size);in(name)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
 #define rep(i,a,b) for(int i=(a);i<(b);++i)
 #define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
 #define each(x,v) for(auto &x:(v))
 #define all(x) (x).begin(),(x).end()
-#define print(x) cout<<(x)<< endl
+#define print(x) cout<<(x)<<endl
 void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
 void solve();
 int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
 
-int N, M;
-int s[10][10];
-int p[10];
-int ans = 0;
-int m[10];
-
-void dfs(int depth) {
-  if (depth == N) {
-    rep(i, 0, M) {
-      int cnt = 0;
-      rep(j, 0, M) {
-        if (s[i][j] == 0) break;
-        cnt += m[s[i][j] - 1];
-      }
-      if (cnt % 2 != p[i]) return;
-    }
-    ans++;
-    return;
-  }
-  m[depth] = 0;
-  dfs(depth + 1);
-  m[depth] = 1;
-  dfs(depth + 1);
-}
-
 void solve() {
-  in(N, M);
+  VAR(int, N, M);
+  vvi s(M);
+
   rep(i, 0, M) {
     VAR(int, k);
-    rep(j, 0, k) in(s[i][j]);
+    rep(j, 0, k) {
+      VAR(int, ss);
+      s[i].push_back(ss);
+    }
   }
-  rep(i, 0, M) in(p[i]);
+  VEC(int, p, M);
 
-  dfs(0);
+  ll ans = 0;
+  rep(b, 0, 1 << N) {
+    int c = 0;
+
+    rep(i, 0, M) {
+      int cc = 0;
+
+      rep(j, 0, (int)s[i].size()) {
+        if ((b >> (s[i][j] - 1)) & 1) {
+          cc++;
+        }
+      }
+
+      if (cc % 2 == p[i]) {
+        c++;
+      }
+    }
+
+    if (c == M) {
+      ans++;
+    }
+  }
+
   print(ans);
 }
