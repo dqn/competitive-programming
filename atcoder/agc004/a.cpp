2aa925f5ccdb5555d29ff3d722312d194a4d67fa
dqn <suyarizzz@gmail.com>
2020-03-30 04:54:04 +0900

    update

---
+++ atcoder/agc004/a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+const ll LINF=0x1fffffffffffffff;
+const int INF=0x3fffffff;
+const int MOD=1000000007;
+#define VAR(type,...) type __VA_ARGS__;in(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);each(x, name)in(x)
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define rrep(i,a,b) for(int i=(a-1);i>=(b);--i)
+#define each(x,v) for(auto &x:(v))
+#define all(x) (x).begin(),(x).end()
+#define print(...) out(__VA_ARGS__);cout<<endl
+void in(){} template<class T,class... U> void in(T &t,U &...u){cin>>t;in(u...);}
+void out(){} template<class T,class... U> void out(T t,U ...u){cout<<t<<" ";out(u...);}
+void solve();
+int main(){cin.tie(0);ios::sync_with_stdio(0);solve();}
+
+ll calc(long double A, long double B, long double C) {
+  return (ceil(B / 2) - (ll)(B / 2)) * A * C;
+}
+
+void solve() {
+  VAR(long double, A, B, C);
+  print(min({calc(A, B, C), calc(B, C, A), calc(C, A, B)}));
+}
